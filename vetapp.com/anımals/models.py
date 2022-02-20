7587c4b19a672aba6d63c9ad6004663701479f1d
UlaşDK <udk97@hotmail.com>
2022-02-20 19:30:48 +0300

    unique  changed

--- vetapp.com/anımals/models.py
+++ vetapp.com/anımals/models.py
@@ -8,10 +8,22 @@ from django.db import models
 
 class Anımals(models.Model):
     name= models.CharField(max_length=30, unique=True)
+    type = models.CharField(max_length=30, unique=False,null=True)
+    species = models.CharField(max_length=30, unique=False,null=True)
+    age = models.IntegerField( unique=False,null=True)
     description= models.TextField(blank=True,null=True)
-    image=models.ImageField(upload_to="anımals/%Y/%m/%d/")
+    # Media part of Model 
+    """ image=models.ImageField(upload_to="anımals/%Y/%m/%d/") """
     date=models.DateTimeField( auto_now=True)
     available=models.BooleanField(default=True)
+    
+    
+
+
 
 def __str__(self):
     return self.name
+
+
+
+""" tür, cins, isim, yaş, açıklama """
